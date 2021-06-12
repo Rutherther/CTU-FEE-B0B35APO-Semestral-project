4567a3e5430890de7b92f2a5b70b23cbbd5c1d38
František Boháček <fandabohacek@gmail.com>
2021-06-12 15:52:33 +0200

    docs: add first description of image viewer

---
+++ docs/auto/frag-master.el
@@ -0,0 +1,25 @@
+(TeX-add-style-hook
+ "frag-master"
+ (lambda ()
+   (TeX-add-to-alist 'LaTeX-provided-package-options
+                     '(("color" "usenames") ("inputenc" "utf8") ("fontenc" "T1") ("ulem" "normalem")))
+   (TeX-run-style-hooks
+    "latex2e"
+    "article"
+    "art10"
+    "color"
+    "inputenc"
+    "fontenc"
+    "graphicx"
+    "grffile"
+    "longtable"
+    "wrapfig"
+    "rotating"
+    "ulem"
+    "amsmath"
+    "textcomp"
+    "amssymb"
+    "capt-of"
+    "hyperref"))
+ :latex)
+
