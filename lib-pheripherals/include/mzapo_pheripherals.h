0b5a39468c7377737e604dd8b75c041275474d71
František Boháček <fandabohacek@gmail.com>
2021-06-18 20:05:37 +0200

    refactor: put all pheripherals to one object

--- lib-pheripherals/include/mzapo_pheripherals.h
+++ lib-pheripherals/include/mzapo_pheripherals.h
@@ -10,12 +10,23 @@
 #ifdef __cplusplus
 extern "C" {
 #endif
+  typedef struct {
+    display_t *display;
+    mzapo_rgb_led_t *rgb_leds;
+    mzapo_ledstrip_t *ledstrip;
+    void **knobs;
+  } mzapo_pheripherals_t;
 
   mzapo_ledstrip_t mzapo_create_ledstrip();
   mzapo_rgb_led_t mzapo_create_rgb_led();
   display_t mzapo_create_display();
 
   void *mzapo_get_knobs_address();
+
+  mzapo_pheripherals_t mzapo_pheripherals_create(mzapo_ledstrip_t *ledstrip, mzapo_rgb_led_t *rgb_led, display_t *display, void **mzapo_knobs_address);
+  bool mzapo_check_pheripherals(mzapo_ledstrip_t *ledstrip,
+                                mzapo_rgb_led_t *rgb_led, display_t *display,
+                                void **mzapo_knobs_address);
 
 #ifdef __cplusplus
 } /* extern "C"*/
--- lib-pheripherals/src/mzapo_pheripherals.c
+++ lib-pheripherals/src/mzapo_pheripherals.c
@@ -47,3 +47,42 @@ void *mzapo_get_knobs_address() {
 
   return pheripherals_base;
 }
+
+mzapo_pheripherals_t mzapo_pheripherals_create(mzapo_ledstrip_t *ledstrip,
+                               mzapo_rgb_led_t *rgb_led, display_t *display,
+                               void **mzapo_knobs_address) {
+  mzapo_pheripherals_t pheripherals = {
+    .display = display,
+    .rgb_leds = rgb_led,
+    .ledstrip = ledstrip,
+    .knobs = mzapo_knobs_address
+  };
+
+  return pheripherals;
+}
+
+bool mzapo_check_pheripherals(mzapo_ledstrip_t *ledstrip,
+                               mzapo_rgb_led_t *rgb_led, display_t *display,
+                               void **mzapo_knobs_address) {
+  #ifdef COMPUTER
+  return true;
+  #endif
+
+  if (ledstrip != NULL && ledstrip->mem_base == NULL) {
+    return false;
+  }
+
+  if (rgb_led != NULL && rgb_led->mem_base == NULL) {
+    return false;
+  }
+
+  if (display != NULL && display->data.base_address == NULL) {
+    return false;
+  }
+
+  if (mzapo_knobs_address != NULL && *mzapo_knobs_address == NULL) {
+    return false;
+  }
+
+  return true;
+}
