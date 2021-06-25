3aa66dc10ef926d48559ca2a9cdf10ee51e0a67a
František Boháček <fandabohacek@gmail.com>
2021-06-25 16:50:17 +0200

    docs: add device mount documentation

--- file-browser/include/device_mount.h
+++ file-browser/include/device_mount.h
@@ -4,9 +4,22 @@
 #include <sys/mount.h>
 #include "file_access.h"
 
+/**
+ * @brief Mount device to target path
+ * 
+ * @param device device to be mounted
+ * @param target folder to mount device to
+ * @return file_operation_error_t
+ */
 file_operation_error_t device_mount(char *device, char *target);
-file_operation_error_t device_umount(char *device, char *target);
 
-char *device_mount_get_filesystem(char *device);
+/**
+ * @brief Unmount device from target path
+ *
+ * @param device device to be unmounted
+ * @param target folder device is mounted to
+ * @return file_operation_error_t
+ */
+file_operation_error_t device_umount(char *device, char *target);
 
 #endif // __MOUNT_UTILS_H__
