93afa062b5f72a12b2c1592477243c5120cf3a06
František Boháček <fandabohacek@gmail.com>
2021-06-29 14:18:22 +0200

    fix: set rgb leds to lower brightness

--- lib-pheripherals/src/mzapo_rgb_led.c
+++ lib-pheripherals/src/mzapo_rgb_led.c
@@ -27,13 +27,13 @@ void rgb_led_set(mzapo_rgb_led_t *rgb_led, mzapo_rgb_leds_t id, uint8_t r,
 }
 
 void rgb_led_set_red(mzapo_rgb_led_t *rgb_led, mzapo_rgb_leds_t id) {
-  rgb_led_set(rgb_led, id, 255, 0, 0);
+  rgb_led_set(rgb_led, id, 30, 0, 0);
 }
 void rgb_led_set_green(mzapo_rgb_led_t *rgb_led, mzapo_rgb_leds_t id) {
-  rgb_led_set(rgb_led, id, 0, 255, 0);
+  rgb_led_set(rgb_led, id, 0, 30, 0);
 }
 void rgb_led_set_blue(mzapo_rgb_led_t *rgb_led, mzapo_rgb_leds_t id) {
-  rgb_led_set(rgb_led, id, 0, 0, 255);
+  rgb_led_set(rgb_led, id, 0, 0, 30);
 }
 
 rgb_led_pixel_t rgb_led_get(mzapo_rgb_led_t *rgb_led, mzapo_rgb_leds_t id) {
